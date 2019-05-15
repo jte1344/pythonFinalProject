ac36aaeb9bfc13f33febcd4c97bba6d0edd05615
areckar2 <50206995+areckar2@users.noreply.github.com>
2019-05-15 11:33:14 -0600

    Update expedia_scrapper.py

--- expedia_scrapper.py
+++ expedia_scrapper.py
@@ -1,68 +1,57 @@
 '''
-This is a tutorial I am running in order to get a better understanding on how to effectively
-parse flight information from a travel site. Once completed and understood, I will personalize
-it and rework it to run on various different sites.
+This scraper will be called from the dates loop for each date for the trip.
+It uses lxml-html to parse the page and populates the needed flight information into a list for each flight on that day.
+It then adds each flight for the day to a list of dictionaries, before sorting them by price and then cutting the list
+to only return the three cheapest flights for the day.
 '''
 
+# import needed modules
 import json
-
 import requests
-
-# Figure out if I can convert this to bs4/beautiful soup
-# This would make it work more in line with what we learned in class
 from lxml import html
 
-from collections import OrderedDict
 
-import argparse
-
-
+# define the parsing function for expedia
 def parse(source, destination, date):
+
+    # Let the parse try to run 5 times before exiting as failed
     for i in range(5):
 
+        # Start block to attempt scrape
         try:
 
-            # The url uses positional argument definitions to adjust the search pattern appropriately
-            # This may work for other sites and we can skip the traversing of pages entirely.
+            # Set the url for calling the scrape
             url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:{0},to:{1},departure:{2}TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com".format(
                 source, destination, date)
 
-            print(url)
+            # set the headers
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-
+            # get the page to scrape
             response = requests.get(url, headers=headers)
 
+            # parse the page
             parser = html.fromstring(response.text)
 
+            # get the data in json format
             json_data_xpath = parser.xpath("//script[@id='cachedResultsJson']//text()")
-
             raw_json = json.loads(json_data_xpath[0] if json_data_xpath else '')
-
             flight_data = json.loads(raw_json["content"])
 
-            flight_info = OrderedDict()
-
+            # initialize the lists to return
             lists = []
 
+            # get info for each leg of the flight
             for i in flight_data['legs'].keys():
 
-                total_distance = flight_data['legs'][i].get("formattedDistance", '')
-
+                # Each piece has a descriptive name to tell what info is being obtained
                 exact_price = flight_data['legs'][i].get('price', {}).get('totalPriceAsDecimal', '')
 
                 departure_location_airport = flight_data['legs'][i].get('departureLocation', {}).get('airportLongName',
                                                                                                      '')
-
                 departure_location_city = flight_data['legs'][i].get('departureLocation', {}).get('airportCity', '')
 
-                departure_location_airport_code = flight_data['legs'][i].get('departureLocation', {}).get('airportCode',
-                                                                                                          '')
-
                 arrival_location_airport = flight_data['legs'][i].get('arrivalLocation', {}).get('airportLongName', '')
-
-                arrival_location_airport_code = flight_data['legs'][i].get('arrivalLocation', {}).get('airportCode', '')
 
                 arrival_location_city = flight_data['legs'][i].get('arrivalLocation', {}).get('airportCity', '')
 
@@ -78,16 +67,19 @@ def parse(source, destination, date):
 
                 flight_days = flight_duration.get('numOfDays', '')
 
+                # Set how many stops are in the flight, this can vary so needs the if/else block
                 if no_of_stops == 0:
 
+                    # Non-stop flight
                     stop = "Nonstop"
 
                 else:
 
+                    # at least one stop in the flight
                     stop = str(no_of_stops) + ' Stop'
 
-                total_flight_duration = "{0} days {1} hours {2} minutes".format(flight_days, flight_hour,
-                                                                                flight_minutes)
+                # continue descriptive information scrape
+                total_flight_duration = "{0} days {1} hours {2} minutes".format(flight_days, flight_hour, flight_minutes)
 
                 departure = departure_location_airport + ", " + departure_location_city
 
@@ -99,16 +91,23 @@ def parse(source, destination, date):
 
                 plane_code = carrier.get('planeCode', '')
 
+                # format the price to a float value with 2 decimals
                 formatted_price = "{0:.2f}".format(exact_price)
 
+                # set airline name only if it hasn't already been set
                 if not airline_name:
                     airline_name = carrier.get('operatedBy', '')
 
+                # Initialize the timeline list for the dict
                 timings = []
 
+                # Get details on the timeline for the flight
                 for timeline in flight_data['legs'][i].get('timeline', {}):
 
+                    # ensure the departure airport is in the keys
                     if 'departureAirport' in timeline.keys():
+
+                        # set flight timing details to info from the scrape
                         departure_airport = timeline['departureAirport'].get('longName', '')
 
                         departure_time = timeline['departureTime'].get('time', '')
@@ -117,6 +116,7 @@ def parse(source, destination, date):
 
                         arrival_time = timeline.get('arrivalTime', {}).get('time', '')
 
+                        # populate the flight timings dictionary
                         flight_timing = {
 
                             'departure_airport': departure_airport,
@@ -129,8 +129,10 @@ def parse(source, destination, date):
 
                         }
 
+                        # add the timings details to the timings list
                         timings.append(flight_timing)
 
+                # populate the flight information dictionary
                 flight_info = {'stops': stop,
 
                                'ticket price': formatted_price,
@@ -153,46 +155,21 @@ def parse(source, destination, date):
 
                                }
 
+                # add the flight details to the list
                 lists.append(flight_info)
 
-            sortedList = sorted(lists, key=lambda k: k['ticket price'], reverse=False)
+            # Sort the list by price
+            sorted_list = sorted(lists, key=lambda k: k['ticket price'], reverse=False)
 
-            cutList = sortedList[:3]
+            # reduce the list to the cheapest 3 flights
+            cut_list = sorted_list[:3]
 
-            return cutList
+            # return the reduced list
+            return cut_list
 
+        # catch if the scrap fails at some point
         except ValueError:
 
             print("Retrying...")
 
-        return {"error": "failed to process the page", }
-
-
-if __name__ == "__main__":
-
-    # Change this main into a function that accepts the args rather than collects them from the terminal line
-    # This will help when I run the program through a double loop in order to get the full range of dates.
-    argparser = argparse.ArgumentParser()
-
-    argparser.add_argument('source', help='Source airport code')
-
-    argparser.add_argument('destination', help='Destination airport code')
-
-    argparser.add_argument('date', help='MM/DD/YYYY')
-
-    args = argparser.parse_args()
-
-    source = args.source
-
-    destination = args.destination
-
-    date = args.date
-
-    print("Fetching flight details")
-
-    scraped_data = parse(source, destination, date)
-
-    print("Writing data to output file")
-
-    with open('%s-%s-flight-results.json' % (source, destination), 'w') as fp:
-        json.dump(scraped_data, fp, indent=4)
+    return {"error": "failed to process the page", }
